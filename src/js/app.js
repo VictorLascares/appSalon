cf9ab874df4e2ad0a92a52cdd05ba05da85ef86e
Victor Lascares <vmlascares@gmail.com>
2021-05-27 16:15:15 -0500

    Creando el HTMl de Servicios

--- src/js/app.js
+++ src/js/app.js
@@ -17,6 +17,26 @@ async function mostrarServicios() {
             const  { id, nombre, precio } = servicio;
             
             // DOM Scripting
+            // Generar nombre de Servicio
+            const nombreServicio = document.createElement('P');
+            nombreServicio.textContent = nombre;
+            nombreServicio.classList.add('nombre-servicio')
+
+            // Generar precio de Servicio
+            const precioServicio = document.createElement('P');
+            precioServicio.textContent = `$${precio}`;
+            precioServicio.classList.add('precio-servicio')
+
+            // Generar contenedor del servicio
+            const servicioDiv = document.createElement('DIV');
+            servicioDiv.classList.add('servicio');
+
+            // Agregar nombreServicio y precioServicio al contenedor
+            servicioDiv.appendChild(nombreServicio);
+            servicioDiv.appendChild(precioServicio);
+
+            // Inyectar en el HTMl
+            document.querySelector('.list-services').appendChild(servicioDiv);
         });
     } catch (error) {
         console.error(error);
